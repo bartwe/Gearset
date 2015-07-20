95aacdaf2b838aab203a164cfa95cfa7cad0a86b
bartwe <bluelive@gmail.com>
2015-07-20 10:12:14 +0200

    Fix naming thingies

--- Gearset/Components/InspectorWPF/NodeTemplateSelector.cs
+++ Gearset/Components/InspectorWPF/NodeTemplateSelector.cs
@@ -8,9 +8,9 @@ using Microsoft.Xna.Framework.Graphics;
 namespace Gearset.Components.InspectorWPF {
     public class NodeTemplateSelector : DataTemplateSelector {
         static readonly Dictionary<Type, CachedTemplate> TypeTemplateMap;
-        static readonly CachedTemplate GenericTemplateCache = new CachedTemplate("genericFieldTemplate");
-        static readonly CachedTemplate GearConfigTemplateCache = new CachedTemplate("gearConfigTemplate");
-        static CachedTemplate _rootTemplateCache = new CachedTemplate("rootTemplate");
+        static readonly CachedTemplate GenericTemplateCache = new CachedTemplate("GenericFieldTemplate");
+        static readonly CachedTemplate GearConfigTemplateCache = new CachedTemplate("GearConfigTemplate");
+        static CachedTemplate _rootTemplateCache = new CachedTemplate("RootTemplate");
 
         /// <summary>
         /// Static constructor
@@ -19,40 +19,40 @@ namespace Gearset.Components.InspectorWPF {
             TypeTemplateMap = new Dictionary<Type, CachedTemplate>();
 
             // Primitive types
-            TypeTemplateMap.Add(typeof(float), new CachedTemplate("floatFieldTemplate"));
-            TypeTemplateMap.Add(typeof(double), new CachedTemplate("floatFieldTemplate"));
-            TypeTemplateMap.Add(typeof(decimal), new CachedTemplate("floatFieldTemplate"));
-            TypeTemplateMap.Add(typeof(char), new CachedTemplate("floatFieldTemplate"));
-            TypeTemplateMap.Add(typeof(short), new CachedTemplate("floatFieldTemplate"));
-            TypeTemplateMap.Add(typeof(ushort), new CachedTemplate("floatFieldTemplate"));
-            TypeTemplateMap.Add(typeof(int), new CachedTemplate("floatFieldTemplate"));
-            TypeTemplateMap.Add(typeof(uint), new CachedTemplate("floatFieldTemplate"));
-            TypeTemplateMap.Add(typeof(long), new CachedTemplate("floatFieldTemplate"));
-            TypeTemplateMap.Add(typeof(ulong), new CachedTemplate("floatFieldTemplate"));
-            TypeTemplateMap.Add(typeof(byte), new CachedTemplate("floatFieldTemplate"));
-            TypeTemplateMap.Add(typeof(sbyte), new CachedTemplate("floatFieldTemplate"));
-            TypeTemplateMap.Add(typeof(bool), new CachedTemplate("boolFieldTemplate"));
+            TypeTemplateMap.Add(typeof(float), new CachedTemplate("FloatFieldTemplate"));
+            TypeTemplateMap.Add(typeof(double), new CachedTemplate("FloatFieldTemplate"));
+            TypeTemplateMap.Add(typeof(decimal), new CachedTemplate("FloatFieldTemplate"));
+            TypeTemplateMap.Add(typeof(char), new CachedTemplate("FloatFieldTemplate"));
+            TypeTemplateMap.Add(typeof(short), new CachedTemplate("FloatFieldTemplate"));
+            TypeTemplateMap.Add(typeof(ushort), new CachedTemplate("FloatFieldTemplate"));
+            TypeTemplateMap.Add(typeof(int), new CachedTemplate("FloatFieldTemplate"));
+            TypeTemplateMap.Add(typeof(uint), new CachedTemplate("FloatFieldTemplate"));
+            TypeTemplateMap.Add(typeof(long), new CachedTemplate("FloatFieldTemplate"));
+            TypeTemplateMap.Add(typeof(ulong), new CachedTemplate("FloatFieldTemplate"));
+            TypeTemplateMap.Add(typeof(byte), new CachedTemplate("FloatFieldTemplate"));
+            TypeTemplateMap.Add(typeof(sbyte), new CachedTemplate("FloatFieldTemplate"));
+            TypeTemplateMap.Add(typeof(bool), new CachedTemplate("BoolFieldTemplate"));
 
             // Other System stuff
-            TypeTemplateMap.Add(typeof(String), new CachedTemplate("stringFieldTemplate"));
-            TypeTemplateMap.Add(typeof(Enum), new CachedTemplate("enumFieldTemplate"));
-            TypeTemplateMap.Add(typeof(void), new CachedTemplate("actionFieldTemplate"));
+            TypeTemplateMap.Add(typeof(String), new CachedTemplate("StringFieldTemplate"));
+            TypeTemplateMap.Add(typeof(Enum), new CachedTemplate("EnumFieldTemplate"));
+            TypeTemplateMap.Add(typeof(void), new CachedTemplate("ActionFieldTemplate"));
 
             // XNA stuff
-            TypeTemplateMap.Add(typeof(Vector2), new CachedTemplate("vector2FieldTemplate"));
-            TypeTemplateMap.Add(typeof(Vector3), new CachedTemplate("vector3FieldTemplate"));
-            TypeTemplateMap.Add(typeof(Quaternion), new CachedTemplate("quaternionFieldTemplate"));
-            TypeTemplateMap.Add(typeof(Texture2D), new CachedTemplate("texture2DFieldTemplate"));
-            TypeTemplateMap.Add(typeof(Color), new CachedTemplate("colorFieldTemplate"));
-            TypeTemplateMap.Add(typeof(Curve), new CachedTemplate("curveFieldTemplate"));
+            TypeTemplateMap.Add(typeof(Vector2), new CachedTemplate("Vector2FieldTemplate"));
+            TypeTemplateMap.Add(typeof(Vector3), new CachedTemplate("Vector3FieldTemplate"));
+            TypeTemplateMap.Add(typeof(Quaternion), new CachedTemplate("QuaternionFieldTemplate"));
+            TypeTemplateMap.Add(typeof(Texture2D), new CachedTemplate("Texture2DFieldTemplate"));
+            TypeTemplateMap.Add(typeof(Color), new CachedTemplate("ColorFieldTemplate"));
+            TypeTemplateMap.Add(typeof(Curve), new CachedTemplate("CurveFieldTemplate"));
 
             // Gearset stuff
-            TypeTemplateMap.Add(typeof(CollectionMarker), new CachedTemplate("collectionFieldTemplate"));
-            TypeTemplateMap.Add(typeof(Texture2DMarker), new CachedTemplate("texture2DMarkerTemplate"));
-            TypeTemplateMap.Add(typeof(LineDrawerConfig), new CachedTemplate("clearableGearConfigTemplate"));
-            TypeTemplateMap.Add(typeof(LabelerConfig), new CachedTemplate("clearableGearConfigTemplate"));
-            TypeTemplateMap.Add(typeof(TreeViewConfig), new CachedTemplate("clearableGearConfigTemplate"));
-            TypeTemplateMap.Add(typeof(PlotterConfig), new CachedTemplate("clearableGearConfigTemplate"));
+            TypeTemplateMap.Add(typeof(CollectionMarker), new CachedTemplate("CollectionFieldTemplate"));
+            TypeTemplateMap.Add(typeof(Texture2DMarker), new CachedTemplate("Texture2DMarkerTemplate"));
+            TypeTemplateMap.Add(typeof(LineDrawerConfig), new CachedTemplate("ClearableGearConfigTemplate"));
+            TypeTemplateMap.Add(typeof(LabelerConfig), new CachedTemplate("ClearableGearConfigTemplate"));
+            TypeTemplateMap.Add(typeof(TreeViewConfig), new CachedTemplate("ClearableGearConfigTemplate"));
+            TypeTemplateMap.Add(typeof(PlotterConfig), new CachedTemplate("ClearableGearConfigTemplate"));
         }
 
         public override DataTemplate SelectTemplate(Object item, DependencyObject container) {
